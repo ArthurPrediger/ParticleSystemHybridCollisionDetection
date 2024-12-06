ba1abd6873a5b2af2ea623a20a5a2c37eb685fcb
Arthur Prediger <arthurprediger@edu.unisinos.br>
2024-12-06 15:10:34 -0300

    Early work for volume structure collision detection and documentation for the particle system code added to the project.

--- Assets/Scripts/ParticleSys.cs
+++ Assets/Scripts/ParticleSys.cs
@@ -9,8 +9,11 @@ public class ParticleSys : MonoBehaviour
     private ComputeShader PSReactionUpdateCS;
     private int kernelIDReacUpdate;
     [SerializeField]
-    private ComputeShader PScreenSpaceCollisionDetectionCS;
-    private int kernelIDSSColDetc;
+    private ComputeShader PSScreenSpaceCollisionDetectionCS;
+    private int kernelIDScrSpaceColDetc;
+    [SerializeField]
+    private ComputeShader PSVolumeStructureCollisionDetectionCS;
+    private int kernelIDVolStructColDetc;
 
     [SerializeField]
     private Material instancedParticlesMat;
@@ -58,56 +61,78 @@ public class ParticleSys : MonoBehaviour
             }
         }
 
+        // Compute buffer IDs setting
         kernelIDReacUpdate = PSReactionUpdateCS.FindKernel("PSReactionUpdate");
-        kernelIDSSColDetc = PScreenSpaceCollisionDetectionCS.FindKernel("PSScreenSpaceCollisionDetection");
+        kernelIDScrSpaceColDetc = PSScreenSpaceCollisionDetectionCS.FindKernel("PSScreenSpaceCollisionDetection");
+        kernelIDVolStructColDetc = PSVolumeStructureCollisionDetectionCS.FindKernel("PSVolumeStructureCollisionDetection");
 
+        // Particles Positions gpu buffer setting
         particlesPosCB = new ComputeBuffer(particlesPos.Count, sizeof(float) * 3);
         particlesPosCB.SetData(particlesPos.ToArray());
 
         instancedParticlesMat.SetBuffer("particlesPos", particlesPosCB);
         PSReactionUpdateCS.SetBuffer(kernelIDReacUpdate, "particlesPos", particlesPosCB);
-        PScreenSpaceCollisionDetectionCS.SetBuffer(kernelIDSSColDetc, "particlesPos", particlesPosCB);
+        PSScreenSpaceCollisionDetectionCS.SetBuffer(kernelIDScrSpaceColDetc, "particlesPos", particlesPosCB);
+        PSVolumeStructureCollisionDetectionCS.SetBuffer(kernelIDVolStructColDetc, "particlesPos", particlesPosCB);
 
+        // Particles Velocities gpu buffer setting
         particlesVelCB = new ComputeBuffer(particlesVel.Count, sizeof(float) * 3);
         particlesVelCB.SetData(particlesVel.ToArray());
 
         PSReactionUpdateCS.SetBuffer(kernelIDReacUpdate, "particlesVel", particlesVelCB);
-        PScreenSpaceCollisionDetectionCS.SetBuffer(kernelIDSSColDetc, "particlesVel", particlesVelCB);
+        PSScreenSpaceCollisionDetectionCS.SetBuffer(kernelIDScrSpaceColDetc, "particlesVel", particlesVelCB);
+        PSVolumeStructureCollisionDetectionCS.SetBuffer(kernelIDVolStructColDetc, "particlesVel", particlesVelCB);
 
+        // Particles without screen space collision detection gpu buffer setting
+        particlesWithoutCollisionCB = new ComputeBuffer(particlesVel.Count, sizeof(float) * 3);
+
+        PSScreenSpaceCollisionDetectionCS.SetBuffer(kernelIDScrSpaceColDetc, "particlesVel", particlesVelCB);
+        PSVolumeStructureCollisionDetectionCS.SetBuffer(kernelIDVolStructColDetc, "particlesWithoutCollision", particlesWithoutCollisionCB);
+
+        // Depth buffer for depth pre-pass setting
         depthTexture = new RenderTexture(Screen.width, Screen.height, 32, RenderTextureFormat.RFloat);
         depthTexture.enableRandomWrite = true;  // Enable random write for compute shader access
         depthTexture.Create();
 
-        PScreenSpaceCollisionDetectionCS.SetTexture(kernelIDSSColDetc, "depthTexture", depthTexture);
+        PSScreenSpaceCollisionDetectionCS.SetTexture(kernelIDScrSpaceColDetc, "depthTexture", depthTexture);
 
+        // Normal buffer for normal pre-pass setting
         normalTexture = new RenderTexture(Screen.width, Screen.height, 32, RenderTextureFormat.ARGBFloat);
         normalTexture.enableRandomWrite = true;  // Enable random write for compute shader access
         normalTexture.Create();
 
-        PScreenSpaceCollisionDetectionCS.SetTexture(kernelIDSSColDetc, "normalTexture", normalTexture);
+        PSScreenSpaceCollisionDetectionCS.SetTexture(kernelIDScrSpaceColDetc, "normalTexture", normalTexture);
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Screen Space Particle Collision setting and dispatch
         DepthPrePass();
         NormalPrePass();
         textureImage.texture = depthTexture;
 
-        PScreenSpaceCollisionDetectionCS.SetMatrix("projectionMat", Camera.main.projectionMatrix);
-        PScreenSpaceCollisionDetectionCS.SetMatrix("viewMat", Camera.main.worldToCameraMatrix);
-        PScreenSpaceCollisionDetectionCS.SetMatrix("inverseProjectionMat", Camera.main.projectionMatrix.inverse);
-        PScreenSpaceCollisionDetectionCS.SetVector("cameraPos", Camera.main.transform.position);
-        PScreenSpaceCollisionDetectionCS.SetFloat("particleRadius", particleRadius);
+        PSScreenSpaceCollisionDetectionCS.SetMatrix("projectionMat", Camera.main.projectionMatrix);
+        PSScreenSpaceCollisionDetectionCS.SetMatrix("viewMat", Camera.main.worldToCameraMatrix);
+        PSScreenSpaceCollisionDetectionCS.SetMatrix("inverseProjectionMat", Camera.main.projectionMatrix.inverse);
+        PSScreenSpaceCollisionDetectionCS.SetVector("cameraPos", Camera.main.transform.position);
+        PSScreenSpaceCollisionDetectionCS.SetFloat("particleRadius", particleRadius);
 
         Vector2 screenRes = new(Screen.width, Screen.height);
-        PScreenSpaceCollisionDetectionCS.SetVector("screenSize", screenRes);
+        PSScreenSpaceCollisionDetectionCS.SetVector("screenSize", screenRes);
 
-        PScreenSpaceCollisionDetectionCS.Dispatch(kernelIDSSColDetc, 1/*particlesPos.Count*/, 1, 1);
+        PSScreenSpaceCollisionDetectionCS.Dispatch(kernelIDScrSpaceColDetc, 1/*particlesPos.Count*/, 1, 1);
 
+        // Volumes Strcuture Particle Collision setting and dispatch
+        PSVolumeStructureCollisionDetectionCS.SetFloat("particleRadius", particleRadius);
+
+        //PSVolumeStructureCollisionDetectionCS.Dispatch(kernelIDVolStructColDetc, 1 , 1, 1);
+
+        // Partcle System reaction update setting and dispatch
         PSReactionUpdateCS.SetFloat(Shader.PropertyToID("deltaTime"), Time.deltaTime);
         PSReactionUpdateCS.Dispatch(kernelIDReacUpdate, 1/*particlesPos.Count*/, 1, 1);
 
+        // Particles mesh instancing rendering
         RenderParams rp = new RenderParams(instancedParticlesMat);
 
         Matrix4x4[] instData = new Matrix4x4[particlesPos.Count];
